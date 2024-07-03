147d3c803bffab4056b08775f3e65386951c7c7e
Jonas Souza <jonasmzsouza@gmail.com>
2024-07-03 17:44:46 -0300

    chore: card grid configuration

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
+import styles from "./page.module.css";
 
 async function getAllPosts() {
   const response = await fetch("http://localhost:3042/posts");
@@ -14,7 +15,7 @@ async function getAllPosts() {
 export default async function Home() {
   const posts = await getAllPosts();
   return (
-    <main>
+    <main className={styles.grid}>
       {posts.map((post) => (
         <CardPost post={post} key={post.id} />
       ))}
