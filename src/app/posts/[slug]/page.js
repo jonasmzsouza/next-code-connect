65b7cba26e01ebdbc55dc5591f02c184f5dca428
Jonas Souza <jonasmzsouza@gmail.com>
2024-07-04 12:06:31 -0300

    chore: dynamic pages - get post

---
+++ src/app/posts/[slug]/page.js
@@ -0,0 +1,23 @@
+import logger from "@/logger";
+
+async function getPostBySlug(slug) {
+  const url = `http://localhost:3042/posts?slug=${slug}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    logger.error("Ops, alguma coisa correu mal");
+    return {};
+  }
+  logger.info("Posts obtidos com sucesso!");
+  const data = await response.json();
+  if (data.length == 0) {
+    return {};
+  }
+  return data[0];
+}
+
+const PagePost = async ({ params }) => {
+  const post = await getPostBySlug(params.slug);
+  return <h1>{post.title}</h1>;
+};
+
+export default PagePost;
